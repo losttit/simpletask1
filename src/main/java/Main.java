acfbf0281a9092a0266a3ebde7439dff0115c914
Andy <pegasin81@gmail.com>
2023-06-16 20:18:25 +0300

    A simple calculator 1.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,21 +5,27 @@ public class Main {
     static String digit;
     public static String calc(String input) {
         String[] expression = input.split(" ");
-        switch (expression[1]){
-            case "+":
-                digit = String.valueOf(Float.parseFloat(expression[0]) + Float.parseFloat(expression[2]));
-                break;
-            case "-":
-                digit = String.valueOf(Float.parseFloat(expression[0]) - Float.parseFloat(expression[2]));
-                break;
-            case "/":
-                digit = String.valueOf(Float.parseFloat(expression[0]) / Float.parseFloat(expression[2]));
-                break;
-            case "*":
-                digit = String.valueOf(Float.parseFloat(expression[0]) * Float.parseFloat(expression[2]));
-                break;
-            default:
-                digit = "Vse xuyina, davai po novoy";
+        int dig1 = Integer.parseInt(expression[0]);
+        int dig2 = Integer.parseInt(expression[2]);
+        if (dig1 >= 1 && dig1 <= 10 && dig2 >= 1 && dig2 <= 10){
+            switch (expression[1]){
+                case "+":
+                    digit = String.valueOf(dig1 + dig2);
+                    break;
+                case "-":
+                    digit = String.valueOf(dig1 - dig2);
+                    break;
+                case "/":
+                    digit = String.valueOf(dig1 / dig2);
+                    break;
+                case "*":
+                    digit = String.valueOf(dig1 * dig2);
+                    break;
+                default:
+                    digit = "Choose one of the signs: +, -, /, *";
+            }
+        } else {
+            digit = "The number is not in the range from 1 to 10";
         }
         return digit;
     }
